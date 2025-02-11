f1cc2aea9c2ff412e8d93439177871843ecd0632
dksqjq4851 <dksqjq4851@naver.com>
2025-02-11 13:41:38 +0900

    02/11

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
+// 중괄호가 쳐져있으면, 메서드를 import. 중괄호가 없으면 해당파일자체를 import
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+
+
+app.use(vuetify)
+app.use(router)
+app.mount('#app')
